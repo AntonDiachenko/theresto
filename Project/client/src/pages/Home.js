d299084dec3c6b89e959c1883c77404baeb6861e
AntonDiachenko <99092120+AntonDiachenko@users.noreply.github.com>
2022-09-19 16:15:21 -0400

    Update Home.js

--- Project/client/src/pages/Home.js
+++ Project/client/src/pages/Home.js
@@ -1,12 +1,20 @@
-import React from "react";
-import { useEffect, useState, useContext } from "react";
+import React from 'react'
+import {useEffect, useState, useContext} from "react";
 import axios from "axios";
-
 import { AuthContext } from "../helper/AuthContext";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom"; 
 
 function Home() {
-  return <div class="container">home</div>;
+
+
+    return (      
+        <div class="container">            
+            home
+           
+            
+        </div>
+           
+      );
 }
 
 export default Home;
