cb8168f9ece27383e6f60e6825d71b0141fc1d6f
sourClout <112970132+sourClout@users.noreply.github.com>
2022-09-19 12:39:43 -0400

    new bootsrap update

--- Project/client/src/index.js
+++ Project/client/src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-// Bootstrap
-import "../bootstrap-5.2.1/dist/css/bootstrap.css";
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
