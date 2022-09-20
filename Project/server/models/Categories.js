70d8723bda9b79c677da6d08faf35f6f466e9357
sourClout <112970132+sourClout@users.noreply.github.com>
2022-09-20 10:01:45 -0400

    tables set
    built first version of all tables in models

---
+++ Project/server/models/Categories.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, Datatypes) => {
+  const Categories = sequelize.define("Categories", {
+    name: {
+      type: Datatypes.STRING(255),
+      allowNull: false,
+      validate: {
+        len: [2, 20],
+      },
+    },
+  });
+
+  // Categories.associate =(models)=>{
+  //     Categories.hasMany(models.Menuitems,{
+  //         onDelete:"cascade",
+  //     })
+  // }
+
+  return Categories;
+};
